190297d389374f9d57a93354aa634161c4ca6f94
adcw <dawaadcw@gmail.com>
2025-01-17 12:54:51 +0100

    config.py: rename present_perc to preserve_perc

--- src/session/datacls/config.py
+++ src/session/datacls/config.py
@@ -4,7 +4,7 @@ from typing import List
 
 @dataclass
 class SplitsCfg:
-    present_perc: float = 0.4
+    preserve_perc: float = 0.4
     max_modalities_to_drop: int = 1
     modality_drop_perc: float = 0.2
 
