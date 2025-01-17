1dabb98408560630d763def9f888592dff40f734
adcw <dawaadcw@gmail.com>
2025-01-17 12:53:44 +0100

    run_dir.py: rename run_dir to __run_dir

--- src/utils/rundir/run_dir.py
+++ src/utils/rundir/run_dir.py
@@ -2,22 +2,22 @@ import os.path
 from datetime import datetime
 
 
-run_dir = None
+__run_dir = None
 
 
 def new_run_dir(root: str):
     if not os.path.exists(root):
         raise OSError(f"Directory {root} does not exist.")
 
-    global run_dir
+    global __run_dir
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    run_dir = os.path.join(root, timestamp)
+    __run_dir = os.path.join(root, timestamp)
 
-    os.makedirs(run_dir)
+    os.makedirs(__run_dir)
 
-    return run_dir
+    return __run_dir
 
 
 def get_run_dir():
-    return run_dir
+    return __run_dir
